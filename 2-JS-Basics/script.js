9d164680c7e436bbda573bdfd2351857eb202e9a
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-07 14:16:15 -0700

    Type coercion y Variable Mutation

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -29,3 +29,24 @@ var jhonMark = "Jhon and Mark";
 /********************************
  * Variable mutation and type coercion
  */
+
+var firstName = 'Jhon';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?', 0);
+console.log(firstName + ' ' + lastName);
