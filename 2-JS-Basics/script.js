4bad8e912327b19251dc27616f157279bd231046
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-07 14:06:06 -0700

    Variables y tipos de datos

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,31 @@
-console.log('Hola Mundo!!!');
+//console.log('Hola Mundo!!!');
+
+/*******************************
+    Variables and data types
+*/
+
+/*
+var firstName = 'Joan';
+console.log(firstName);
+
+var lastName = 'García';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+var job = 'Teacher';
+console.log(job);
+
+//Variable naming rules
+var _3years = 3;
+var jhonMark = "Jhon and Mark";
+//var if = 30;
+*/
+
+/********************************
+ * Variable mutation and type coercion
+ */
