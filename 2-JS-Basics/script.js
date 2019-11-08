737b81c81955a61536bc3f85cbb596f8a4072381
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-07 22:32:33 -0700

    Ternary Operator y Switch Statement

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -189,7 +189,7 @@ if(johnBMI > markBMI){
 
 /*****************************
  * Boolean Logic
- */
+ 
 
  var firstName = 'John';
  var age = 20;
@@ -203,3 +203,48 @@ if(johnBMI > markBMI){
 }else{
     console.log(firstName + ' is a man.')
  }
+ */
+
+ /****************************
+  * The ternary operator and switch statements
+  */
+
+var firstName = 'John';
+var age = 16;
+
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+//Stwitfch Statement
+var job = 'instructor';
+switch(job){
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+age = 56
+switch(true){
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
