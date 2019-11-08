2dfe2a3901ea74b595525ff2b62e908d4c2ce300
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-08 15:24:24 -0700

    Funciones

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -318,3 +318,33 @@ if(johnAverage > mikeAverage && johnAverage > maryAverage){
 
 */
 
+/***************************
+ * Functions
+ */
+
+ function calculateAge(birthYear){
+     return 2019 - birthYear;
+ }
+
+var ageJhon = calculateAge(1991);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJhon, ageMike, ageJane);
+
+
+function yearsUntilRetirement(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if(retirement > 0){
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    }else{
+        console.log(firstName + ' is already retired');
+    }
+
+    
+}
+
+yearsUntilRetirement(1991, 'Joan');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
