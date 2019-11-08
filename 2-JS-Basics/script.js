34a5034d09544b11fdb2ccad881a92d77fdc736c
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-08 15:59:26 -0700

    Arrays

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -352,7 +352,7 @@ yearsUntilRetirement(1969, 'Jane');
 
 /**************************************
  * Function Statements and Expressions
- */
+ 
 
 //Function declaration
 //function whatDoYouDo(job, firstName){}
@@ -374,3 +374,37 @@ var whatDoYouDo = function(job, firstName){
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('retired', 'Mike'));
 console.log(whatDoYouDo('designer', 'Jane'));
+*/
+
+/******************************
+ * Arrays
+ */
+
+ var names = ['John', 'Mike', 'Jane'];
+ var years = new Array(1990, 1969, 1948);
+
+ console.log(names[2]);
+ console.log(names.length);
+
+ //Mutate array data
+ names[1] = 'Ben';
+ names[names.length] = 'Mary';
+ console.log(names);
+
+ //Different data types
+ var john = ['John', 'Smith', 1990, 'designer', false];
+
+ john.push('blue');
+ john.unshift('Mr.');
+ console.log(john);
+ 
+ john.pop();
+ john.pop();
+ john.shift();
+ console.log(john);
+
+ console.log(john.indexOf(1990));
+
+
+ var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+ console.log(isDesigner);
