144141cfec338b7e3e74ca8181164fa8b99b19b0
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-09 20:07:09 -0700

    continue, break and lopping backwards

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -562,7 +562,6 @@ if (john.bmi < mark.bmi) {
 
   /*******************************
    * Loops and iteration
-   */
 
    // For Loop
 for(var i = 0; i < 10; i++){
@@ -580,4 +579,25 @@ var i = 0;
 while(i < john.length){
     console.log(john[i]);
     i++;
+}
+
+   */
+
+// continue and break statements
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+for (var i = 0; i < john.length; i++) {
+    if(typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+//Looping backwards
+for(var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
 }
