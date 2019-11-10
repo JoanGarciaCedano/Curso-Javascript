3fade6f639f8046cb7de2cc0992e91c1e727cbb3
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-09 22:01:35 -0700

    The this keyword

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -32,7 +32,7 @@ console.log(age);
 
 /**************************************
  * Scoping
- */
+ 
 
  //First scoping example
 
@@ -69,3 +69,45 @@ function third(){
     //console.log(c);
     console.log(a, d);
 }
+
+*/
+
+/**********************************+
+ * The this keyword
+ */
+
+ //console.log(this);
+
+//  calculateAge(1985);
+
+//  function calculateAge(year){
+//      console.log(2016 - year);
+//      console.log(this);
+//  }
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function(){
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+
+        // function innerFunction(){
+        //     console.log(this);
+        // }
+
+        // innerFunction();
+
+    }
+};
+
+john.calculateAge();
+
+var mike ={
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+
+mike.calculateAge();
