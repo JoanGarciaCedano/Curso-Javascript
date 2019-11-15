99c677ecc1de6cccfffeb62f5a552c53a630e7b9
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-15 14:57:01 -0700

    Functions returning Functions

--- 05-Advanced-JS/script.js
+++ 05-Advanced-JS/script.js
@@ -81,7 +81,7 @@ function change(a, b){
 change(age,obj);
 
 console.log(age, obj.city);
-*/
+
 
 ///////////////////////
 //Lecture: Passing functions as arguments
@@ -120,3 +120,31 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+
+
+////////////////////////////////////////////
+/// Lecture: Functions returning functions
+
+function interviewQuestion(job){
+    if(job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    }else if(job === 'teacher'){
+        return function(name){
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    }else{
+        return function(name){
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+designerQuestion('Joan');
+
+interviewQuestion('teacher')('Mark');
+*/
