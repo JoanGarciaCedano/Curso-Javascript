ccf2259b94b4fa2e213b1a371d63659ce1429023
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-19 16:49:53 -0700

    Clearing our fields

--- 06-Budgety/app.js
+++ 06-Budgety/app.js
@@ -101,6 +101,19 @@ var UIController = (function(){
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+        clearFields: function(){
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+            var fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -136,6 +149,8 @@ var controller = (function(budgetCtrl, UICtrl){
         var newItem = budgetController.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI 
         UIController.addListItem(newItem, input.type);
+        // 4. Crear the fields
+        UIController.clearFields();
         // 4. Calculate the budget 
 
         // 5. Display the budget on the UI
