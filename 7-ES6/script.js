8313a9075927930d9268f430e465c4f443ab3eb1
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-23 18:55:37 -0700

    ES5 - Arrow Functions 'This' Keyword

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -102,26 +102,44 @@
 /////////////////////////////////////////////
 // Lecture: Arrow Functions
 
-const years = [1990, 1965, 1982, 1937];
+// const years = [1990, 1965, 1982, 1937];
+
+// // ES5
+// var ages5 = years.map(function(el){
+//     return 2016 - el;
+// })
+// console.log(ages5);
+
+
+// // ES6
+// let ages6 = years.map(el => 2016 - el);
+// console.log(ages6);
+
+
+// ages6 = years.map((el, index) => `Age element ${index + 1}: ${el}`);
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => {
+//     const now = new Date().getFullYear();
+//     const age = now - el;
+//     return `Age element ${index + 1}: ${age}`;
+// });
+// console.log(ages6);
+
+//////////////////////////////////////////////
+// Lecture: Arrow functions 2
 
 // ES5
-var ages5 = years.map(function(el){
-    return 2016 - el;
-})
-console.log(ages5);
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function(){
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        })
+    }
+}
 
-
-// ES6
-let ages6 = years.map(el => 2016 - el);
-console.log(ages6);
-
-
-ages6 = years.map((el, index) => `Age element ${index + 1}: ${el}`);
-console.log(ages6);
-
-ages6 = years.map((el, index) => {
-    const now = new Date().getFullYear();
-    const age = now - el;
-    return `Age element ${index + 1}: ${age}`;
-});
-console.log(ages6);
+box5.clickMe();
