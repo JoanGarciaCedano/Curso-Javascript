9851599c695c7ce39e66d8c96f162b279117f906
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-23 17:59:00 -0700

    ES6 - Blocks and IIFEs

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -49,3 +49,24 @@
 // }
 
 // console.log(i);
+
+
+///////////////////////////////////////////////////////
+// Lecture: Blocks and IIFEs
+
+// ES 6
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+//console.log(a, b);
+console.log(c);
+
+// ES5
+(function(){
+    var c = 3;
+})();
+
+//console.log(c);
