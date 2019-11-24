0625b8038e00f9540aa6fc34faccc4025a09ad44
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-23 18:16:20 -0700

    ES6 - Strings

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -54,19 +54,47 @@
 ///////////////////////////////////////////////////////
 // Lecture: Blocks and IIFEs
 
-// ES 6
-{
-    const a = 1;
-    let b = 2;
-    var c = 3;
+// // ES 6
+// {
+//     const a = 1;
+//     let b = 2;
+//     var c = 3;
+// }
+
+// //console.log(a, b);
+// console.log(c);
+
+// // ES5
+// (function(){
+//     var c = 3;
+// })();
+
+// //console.log(c);
+
+///////////////////////////////////////////////////////////
+// Lecture: Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year){
+    return 2019 - year;
 }
 
-//console.log(a, b);
-console.log(c);
-
 // ES5
-(function(){
-    var c = 3;
-})();
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + 
+            '. Today, he is ' +calcAge(yearOfBirth) + ' years old.');
 
-//console.log(c);
+
+// ES6
+console.log(`This is ${firstName} 
+            ${lastName}. He was born in 
+            ${yearOfBirth}. Today, he is 
+            ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));
+console.log(n.endsWith('th'));
+console.log(n.includes(' '));
+console.log(`${firstName} `.repeat(5));
