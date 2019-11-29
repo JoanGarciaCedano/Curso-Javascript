455f38de4f12911bb2b3430768d0476466543a19
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-28 21:45:40 -0700

    Array.from ES6

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -245,8 +245,11 @@
 
 const boxes = document.querySelectorAll('.box');
 
-//ES5
-var boxesArr5 = Array.prototype.slice.call(boxes);
-boxesArr5.forEach(function(cur){
-    cur.style.backgroundColor = 'dodgerblue';
-});
+// //ES5
+// var boxesArr5 = Array.prototype.slice.call(boxes);
+// boxesArr5.forEach(function(cur){
+//     cur.style.backgroundColor = 'dodgerblue';
+// });
+
+//ES6
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
