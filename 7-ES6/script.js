f2e4b503d035ba56bdb873c93cddb92e32404408
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-28 21:57:52 -0700

    Array indexOf in ES5

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -265,8 +265,19 @@ boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
 // ES6 forof
 for(const cur of boxesArr6){
-    if(cur.className === 'box blue'){
+    if(cur.className.includes('blue')){
         continue;
     }
     cur.textContent = 'I changed in ES6';
 }
+
+//ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(cur){
+    return cur >= 18;
+});
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
