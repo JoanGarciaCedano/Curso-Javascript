8931ed6725085a04de20b3bc4d970e8ef2326daf
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-28 21:53:35 -0700

    Array.className ES6 and continue

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -245,19 +245,28 @@
 
 const boxes = document.querySelectorAll('.box');
 
-//ES5
-var boxesArr5 = Array.prototype.slice.call(boxes);
-boxesArr5.forEach(function(cur){
-    cur.style.backgroundColor = 'dodgerblue';
-});
+// //ES5
+// var boxesArr5 = Array.prototype.slice.call(boxes);
+// boxesArr5.forEach(function(cur){
+//     cur.style.backgroundColor = 'dodgerblue';
+// });
 
-//ES6
-Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+// //ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
-//ES5
-for(var i = 0; i < boxesArr5.length; i++){
-    if(boxesArr5[i].className === 'box blue'){
+// //ES5
+// for(var i = 0; i < boxesArr5.length; i++){
+//     if(boxesArr5[i].className === 'box blue'){
+//         continue;
+//     }
+//     boxesArr5[i].textContent = 'I changed to blue';
+// }
+
+// ES6 forof
+for(const cur of boxesArr6){
+    if(cur.className === 'box blue'){
         continue;
     }
-    boxesArr5[i].textContent = 'I changed to blue';
+    cur.textContent = 'I changed in ES6';
 }
