b3d6dd6ce24faf4ad7bec09804e08d410e92f621
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-11-30 21:52:33 -0700

    ES6 - Rest parameters example 2

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -295,7 +295,7 @@
 
 // var sum1 = addFourAges(18, 30, 12, 21);
 // console.log(sum1);
-
+/*
 // //ES5
 // var ages = [18, 30, 12, 21];
 // var sum2 = addFourAges.apply(null, ages);
@@ -340,3 +340,24 @@ function isFullAge6(...years){
 }
 
 isFullAge6(1990, 1999, 1965, 2016, 1987);
+*/
+
+// ES5
+function isFullAge5(limit) {
+    //console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+
+    argsArr.forEach(function (cur) {
+        console.log((2016 - cur) >= limit);
+    });
+}
+
+//isFullAge5(16, 1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965,2016, 1987);
+
+// ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2016 - cur) >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
