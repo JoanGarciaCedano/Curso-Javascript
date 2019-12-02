88d5ae31e9d0dce699173b74baca9ce2bba84784
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-12-01 17:35:45 -0700

    ES6 - Defaul parameters example

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -365,15 +365,28 @@ isFullAge6(1990, 1999, 1965, 2016, 1987);
 ///////////////////////////////////////////////////
 // Lecture: Default parameters
 
-function SmithPerson(firstName, yearOfBirth, lastName, nationality){
+// //ES5
+// function SmithPerson(firstName, yearOfBirth, lastName, nationality){
 
-    lastName === undefined ? lastName = 'Smith' : lastName = lastName;
-    nationality === undefined ? nationality = 'american' : nationality = nationality;
+//     lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+//     nationality === undefined ? nationality = 'american' : nationality = nationality;
 
 
+//     this.firstName = firstName;
+//     this.yearOfBirth = yearOfBirth;
+//     this.lastName = lastName;
+//     this.nationality = nationality;
+// }
+
+// var john = new SmithPerson('John', 1990);
+// var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
+
+//ES6
+
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american'){
     this.firstName = firstName;
-    this.yearOfBirth = yearOfBirth;
     this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
     this.nationality = nationality;
 }
 
