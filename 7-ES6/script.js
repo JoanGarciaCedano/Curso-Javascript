5867e8d1784a1bad0abfefcf194c52982e07b9f4
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-12-01 17:47:18 -0700

    ES6 - MAPS declaration example

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -392,3 +392,16 @@ isFullAge6(1990, 1999, 1965, 2016, 1987);
 
 // var john = new SmithPerson('John', 1990);
 // var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
+
+////////////////////////////////////////////////////////
+// Lecture: Maps
+
+const question = new Map();
+question.set('question', 'What is the official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer');
+question.set(false, 'Wrong, prease try again');
