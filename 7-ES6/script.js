566053e49fddba5a2d5de2093536f665c2c9b628
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-12-01 17:31:49 -0700

    ES5 - Default parameters

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -342,22 +342,39 @@ function isFullAge6(...years){
 isFullAge6(1990, 1999, 1965, 2016, 1987);
 */
 
-// ES5
-function isFullAge5(limit) {
-    //console.log(arguments);
-    var argsArr = Array.prototype.slice.call(arguments, 1);
+// // ES5
+// function isFullAge5(limit) {
+//     //console.log(arguments);
+//     var argsArr = Array.prototype.slice.call(arguments, 1);
 
-    argsArr.forEach(function (cur) {
-        console.log((2016 - cur) >= limit);
-    });
+//     argsArr.forEach(function (cur) {
+//         console.log((2016 - cur) >= limit);
+//     });
+// }
+
+// //isFullAge5(16, 1990, 1999, 1965);
+// // isFullAge5(1990, 1999, 1965,2016, 1987);
+
+// // ES6
+// function isFullAge6(limit, ...years) {
+//     years.forEach(cur => console.log((2016 - cur) >= limit));
+// }
+
+// isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+
+///////////////////////////////////////////////////
+// Lecture: Default parameters
+
+function SmithPerson(firstName, yearOfBirth, lastName, nationality){
+
+    lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+    nationality === undefined ? nationality = 'american' : nationality = nationality;
+
+
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
 }
 
-//isFullAge5(16, 1990, 1999, 1965);
-// isFullAge5(1990, 1999, 1965,2016, 1987);
-
-// ES6
-function isFullAge6(limit, ...years) {
-    years.forEach(cur => console.log((2016 - cur) >= limit));
-}
-
-isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+var john = new SmithPerson('John', 1990);
